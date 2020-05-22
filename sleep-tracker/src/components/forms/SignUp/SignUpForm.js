ddcebd926886ace41942b21b7d0724d7d76cfcc1
Mysti Dyse <mystidyse@gmail.com>
2020-05-21 18:46:13 -0700

    final push of the day

---
+++ sleep-tracker/src/components/forms/SignUp/SignUpForm.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+const SignUpForm = (signin) => {
+    /* state */
+    const [input, setInput] = useState({
+        id: Date.now(),
+        
+    })
+}
